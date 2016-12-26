760d63ab52c78dacf4e3ef89ecdbbd945c6b72d5
Connor Kennelly <knelly26@gmail.com>
2016-12-25 18:22:55 -0800

    Add 2 layout pages, modify landing page, fill in content on About.

---
+++ SpartanClash/SpartanClash/Controllers/LeaderboardsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpartanClash.Controllers
+{
+    public class LeaderboardsController : Controller
+    {
+        // GET: Leaderboards
+        public ActionResult Leaderboards()
+        {
+            return View();
+        }
+    }
+}
---
+++ SpartanClash/SpartanClash/Controllers/ServiceRecordController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpartanClash.Controllers
+{
+    public class ServiceRecordController : Controller
+    {
+        // GET: ServiceRecord
+        public ActionResult ServiceRecord()
+        {
+            return View();
+        }
+    }
+}
